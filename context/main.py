ef9573fb42d02b3e626da6a26120715808598074
sadafshahab12 <sadafshahab123@yahoo.com>
2025-09-18 09:36:45 +0500

    Removed .env from repository

--- context/main.py
+++ context/main.py
@@ -31,16 +31,31 @@ async def fetch_user_age(wrapper: RunContextWrapper[UserInfo]):
     return f"User {wrapper.context.name} is 47 years old."
 
 
+weather_api_key = os.getenv("WEATHER_API_KEY")
+
+
+@function_tool
+def fetch_weather_tool(city):
+    """fetch the weather"""
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric"
+
+
 async def main():
     # create your context object
     user_info = UserInfo(name="john", uid=123)
     client = AsyncOpenAI(api_key=GEMINI_API_KEY, base_url=BASE_URL)
     model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=client)
-    agent = Agent[UserInfo](name="Assistant", tools=[fetch_user_age], model=model)
+    agent = Agent[UserInfo](
+        name="Assistant",
+        tools=[fetch_user_age],
+        instructions=f"The user’s name is {user_info.name}, and their ID is {user_info.uid}. "
+        f"If asked about age, you may use the fetch_user_age tool.",
+        model=model,
+    )
 
     result = await Runner.run(
         starting_agent=agent,
-        input="What is the age of the user?",
+        input="Who is the user?",
         context=user_info,
     )
 
